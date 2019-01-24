ae97d1033650b7b0852c46617590f5e932f9bc21
NORISA ERLIANA V <norisa.erlin@gmail.com>
2019-01-23 17:47:27 -0800

    Tambah Commit

---
+++ src/Persegi.java
@@ -0,0 +1,19 @@
+public class Persegi {
+
+
+
+    private double phi = 22 / 7;
+    private double r = 2;
+    private double Luas;
+
+
+    public void LuasLingkaran ( ){
+
+        Luas = phi * r *r ;
+
+        System.out.println( "Luas " + Luas);
+
+
+    }
+
+}
